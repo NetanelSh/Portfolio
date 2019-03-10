9bb12579e31ad4066cf68437508ec2e7f3a89a83
Netanel Sheinbin <sheinbinn@gmail.com>
2019-03-10 19:43:03 +0200

    portfolio model

--- server/models/portfolio.js
+++ server/models/portfolio.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const setStringType = (maxlength) => { 
+    return { type: String, required: true, maxlength: maxlength } 
+};
+
 const portfolioScheme = new Schema({
-    userId: String,
-    title: String,
-    company: String,
-    location: String,
-    position: String,
-    description: String,
-    startDate: Date,
-    endDate: Date
+    userId: setStringType(512),
+    title: setStringType(256),
+    company: setStringType(256),
+    location: setStringType(128),
+    position: setStringType(256),
+    description: setStringType(2048),
+    startDate: {type: Date, required: true},
+    endDate: {type: Date, required: true}
 });
 
 module.exports = mongoose.model('Portfolio', portfolioScheme);
